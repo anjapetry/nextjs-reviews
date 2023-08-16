bed225c78a3af95eb3e8d0ba7abf8da0d7af6687
AnjaP <treyjapan@pm.me>
2023-08-16 19:54:04 +0200

    feat:add homepage folder and update root layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,13 @@ export default function RootLayout({ children}) {
     return (
     <html lang="en">
         <body>
+            <header>
+                [header]
+            </header>
             {children}
+            <footer>
+                [footer]
+            </footer>
             </body>
         </html>
     );
---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+    return (
+        <>
+        <h1>Indie Music Reviews</h1>
+        <p>
+            Welcome to Indie Music Reviews! We are a site dedicated to reviewing the best and latest indie music out there.
+        </p>
+        </>
+    );
+};

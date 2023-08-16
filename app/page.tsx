3cf429c73090aa08cfc1e9497caa7a2a3cc1e4e0
AnjaP <treyjapan@pm.me>
2023-08-16 20:34:27 +0200

    feat:create reviews page and update package.json

--- app/page.tsx
+++ app/page.tsx
@@ -8,3 +8,5 @@ export default function HomePage() {
         </>
     );
 };
+
+// => http://localhost:3000/
---
+++ app/reviews/page.tsx
@@ -0,0 +1,13 @@
+export default function ReviewsPage() {
+    return (
+        <>
+        <h1>Album Reviews</h1>
+        <p>
+            Here are some of our latest album reviews. Be sure to check back often for new reviews! And feel free  to <a href="/contact">contact us</a> if you have any suggestions for albums you'd like to see reviewed.
+            We'd love to hear your thoughts on any of the albums we've reviewed. You can leave a comment on any of the album pages.
+        </p>
+        </>
+    );
+};
+
+// => http://localhost:3000/reviews/

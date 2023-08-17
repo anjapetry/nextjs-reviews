161126015ac81179d554c9c1de71aac87a0aa545
AnjaP <treyjapan@pm.me>
2023-08-18 00:36:51 +0200

    feat:create nav in root layout and add links for routing

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,8 +2,7 @@ export default function AboutPage() {
     return (
         <>
         <h1>About</h1>
-        <p>Hi, I'm Anja, your reviewer of trust here on 
-            <a href="https://indiemusicreviews.com">Indie Music Reviews</a>.
+        <p>Hi, I'm Anja, your trusted hobbyist reviewer here on Indie Music Reviews. Besides being a life-long music lover, an avid reader and an aspiring writer, I'm also a big fan of (play)lists, so you'll find plenty of those here. You'll also find a lot of indie music, of course, but I'll also be reviewing some jazz and jazz-adjacent material from time to time. I hope you enjoy my reviews and lists, and feel free to send me your suggestions for new darlings to <a href="/contact">contact us</a>!
         </p>
         </>
     )
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import Link from "next/link";
 
 interface LayoutProps {
     children: ReactNode;
@@ -7,9 +8,29 @@ interface LayoutProps {
 export default function RootLayout({ children }: LayoutProps) {
     return (
     <html lang="en">
+        <head>
+            <meta charSet="utf-8" />
+            <title>Indie Music Reviews</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </head>
         <body>
             <header>
-                [header]
+                <nav>
+                    <ul>
+                        <li>
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link href="/reviews">Reviews</Link>
+                        </li>
+                        <li>
+                            <Link href="/darlings">Darlings</Link>
+                        </li>
+                        <li>
+                            <Link href="/about">About</Link>
+                        </li>
+                    </ul>
+                </nav>
             </header>
             {children}
             <footer>
